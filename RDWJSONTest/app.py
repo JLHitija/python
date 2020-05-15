2d4ea95a40af48e30e34c8a110f9726391ead1c9
Jessy <liewesjessy@gmail.com>
2020-05-15 10:36:13 +0200

    Sommige dingen duurde lang om achter te komen (staat als comment in de code) maar ik vind het een leuke leerzame opdracht. Heb ook een To-do list erin gezet zodat je een beetje een idee hebt van wat ik er uiteindelijk van wil maken. Ik heb het wel zo in mn hoofd dat dit nog gewoon een beetje testen en uitproberen is, want ik vind t nog wel lastig (ook omdat ik lang weinig heb gedaan) dus ik denk dat dit een goeie voorbereiding is op het uiteindelijke project. UPDATE: Je kan nu op kenteken zoeken. UPDATE: je kan nu op model zoeken.

--- RDWJSONTest/app.py
+++ RDWJSONTest/app.py
@@ -6,21 +6,27 @@
 import json
 import requests
 import sys
+import pprint
 
 exit = False
+pp = pprint.PrettyPrinter(indent=4)
 
 def search():
-    print("Op wat wilt u zoeken?\nmerk | kenteken | exit")
+    print("Op wat wilt u zoeken?\nmodel | kenteken | exit")
     entry = input(">")
-    if entry == "merk":
-        print("Coming Soon.")
+    if entry == "model":
+        merk = input("model(TYPE IN CAPS):")
+        api = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?handelsbenaming="
+        data = "".join([api, merk])
+        data2 = requests.get(data).json()
+        pp.pprint(data2)
 
     elif entry == "kenteken":
         kenteken = input("kenteken:")
         api = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken="
         data = "".join([api, kenteken])
-        kdata = requests.get(data).json()
-        print(kdata)
+        data2 = requests.get(data).json()
+        pp.pprint(data2)
 
     elif entry == "exit":
         exit = True
